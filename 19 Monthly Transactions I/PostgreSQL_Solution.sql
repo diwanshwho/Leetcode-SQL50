38cd97890c9db7dca68b78eab739e07d7fb402d5
Diwanshu Sharma <132468837+diwanshwho@users.noreply.github.com>
2024-07-29 20:54:58 +0530

    Added PostgreSQL Solution

---
+++ 19 Monthly Transactions I/PostgreSQL_Solution.sql
@@ -0,0 +1,4 @@
+SELECT TO_CHAR(trans_date, 'YYYY-MM') AS month, country, COUNT(state) AS trans_count, 
+COUNT(state) FILTER (WHERE state= 'approved') AS approved_count, SUM(amount) AS trans_total_amount, COALESCE(SUM(amount) FILTER (WHERE state='approved'),0) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country;
